5fd8484173bb9c9674a87e8322311fd08333b229
Rohama Salfi <153669350+rohxama@users.noreply.github.com>
2024-08-05 14:11:50 -0700

    Day 2

---
+++ Day2.js
@@ -0,0 +1,19 @@
+// Interview Questions
+
+
+// 3- How to empty an array without using loop or without reseting it?
+
+
+let arr = [1, 2, 3, 4, 5];
+arr.length = 0;
+console.log(arr); // []
+
+// 4- How to check is it integer or not?
+
+
+let int = 3;
+if (int % 1 === 0){
+    console.log('Yes, it is integer'); 
+}else{
+    console.log('No way');
+}
